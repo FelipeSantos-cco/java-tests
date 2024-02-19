80d16362eed42bbfa5ae74eee8bac886bb981a0f
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-02-19 08:51:28 -0300

    :star:Add Project: Training API with Spring Boot

---
+++ Spring-Boot/training-api/src/main/java/school/sptech/trainingapi/TrainingApiApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.trainingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingApiApplication.class, args);
+	}
+
+}
