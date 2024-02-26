eb0d5a4db3acddff915f29b2c17a141c9d1e6eaf
FelipeSantos-cco <felipe.almeida@sptech.school>
2024-02-26 12:07:12 -0300

    :sparkles: Feat: Bubble Sort Simple

--- Data-Structure/bubble-sort/src/main/java/school/sptech/BubbleSort.java
+++ Data-Structure/bubble-sort/src/main/java/school/sptech/BubbleSort.java
@@ -1,7 +1,21 @@
 package school.sptech;
 
 public class BubbleSort {
-    public static int[] ordenar(int[] vetorBaguncado){
-        
+    public static int[] ordenarVetorInt(int[] vetor){
+        int numeroTemporario;
+
+        for (int i = 0; i < vetor.length - 1; i++) {
+            for (int j = 0; j < vetor.length - i - 1; j++) {
+
+                if(vetor[j] > vetor[j + 1]){
+
+                    numeroTemporario = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = numeroTemporario;
+                }
+            }
+        }
+
+        return vetor;
     }
 }
--- Data-Structure/bubble-sort/src/main/java/school/sptech/Main.java
+++ Data-Structure/bubble-sort/src/main/java/school/sptech/Main.java
@@ -1,8 +1,23 @@
 package school.sptech;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+        int[] vetorInt = {2, 3, 45, 4, 23, 12, 45, 3};
 
+        System.out.println("Vetor NÃO ordenado");
+        exibeVetorInt(vetorInt);
 
+        System.out.println("\nVetor ORDENADO");
+        exibeVetorInt(BubbleSort.ordenarVetorInt(vetorInt));
+        
+        
+    }
+    
+    public static void exibeVetorInt(int[] vetor){
+        for (int i : vetor) {
+            System.out.print(i + " ");
+        }
     }
 }
